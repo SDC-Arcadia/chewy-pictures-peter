6f3845312563976f41548cf7741d7ec5880d2e64
Dominic Silvia <dominic.silvia@gmail.com>
2020-08-09 15:32:58 -0700

    install styled-components and add grid css

--- client/components/Photos.jsx
+++ client/components/Photos.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import S3_URL from '../lib/s3.js';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: .5fr 6fr .5fr;
+  grid-template-areas:
+    "up main"
+    "thumbs main"
+    "thumbs main"
+    "thumbs main"
+    "thumbs main"
+    "thumbs main"
+    "thumbs main"
+    "down zoom"
+`;
+
+const Thumbnails = styled.div`
+  background: yellow;
+  grid-area: thumbs;
+`;
+
+const Photo = styled.div`
+  background: green;
+  grid-area: main;
+
+`;
 
 export default class Photos extends React.Component {
   constructor(props) {
