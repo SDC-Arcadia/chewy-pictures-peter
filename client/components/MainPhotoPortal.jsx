3fb6aba7aed189ab2c14b3f4e809690532b56655
Dominic Silvia <dominic.silvia@gmail.com>
2020-08-22 11:17:54 -0700

    complete basic layout of portal

--- client/components/MainPhotoPortal.jsx
+++ client/components/MainPhotoPortal.jsx
@@ -9,9 +9,10 @@ const PortalStyle = styled.div`
   top: 0;
   left: 0;
   display: grid;
-  grid-template-rows: auto min-content;
+  grid-template-rows: min-content auto min-content;
   grid-template-columns: min-content auto min-content;
-  grid-template-areas: "prev main next"
+  grid-template-areas: "close close close"
+                       "prev main next"
                        "desc desc desc";
   align-items: center;
   justify-content: center;
@@ -23,7 +24,7 @@ const ImageStyle = styled.img`
   width: 600px
 `;
 
-const PrevStyle = styled.button`
+const ZoomedPrevStyle = styled.button`
   grid-area: prev;
 
   width: 58px;
@@ -42,11 +43,51 @@ const PrevStyle = styled.button`
   border: none;
 `;
 
+const ZoomedNextStyle = styled.button`
+  grid-area: next;
+
+  width: 58px;
+  height: 58px;
+  background: url('https://rpt22-fec-kwame.s3-us-west-1.amazonaws.com/images/static/arrow-right-white.svg') no-repeat;
+  ${'' /* background-attachment: scroll;
+  background-origin: padding-box;
+  background-clip: border-box; */}
+  background-color: transparent;
+  background-size: 15px 26px;
+  display: block;
+  position: relative;
+  align-text: right;
+  justify-self: end;
+  ${'' /* cursor: pointer; */}
+  border: none;
+`;
+
+const ZoomedCloseStyle = styled.button`
+  grid-area: close;
+
+  width: 58px;
+  height: 58px;
+  background: url('https://rpt22-fec-kwame.s3-us-west-1.amazonaws.com/images/static/icon-close-white.svg') no-repeat;
+  ${'' /* background-attachment: scroll;
+  background-origin: padding-box;
+  background-clip: border-box; */}
+  background-color: transparent;
+  background-size: 15px 26px;
+  ${'' /* display: flex; */}
+  ${'' /* position: relative; */}
+  ${'' /* align-text: right; */}
+  justify-self: end;
+  ${'' /* cursor: pointer; */}
+  border: none;
+`;
+
 
 const MainPhotoPortal = ({ photo, onClick }) => (
   <PortalStyle onClick={onClick}>
-    <PrevStyle />
+    <ZoomedPrevStyle />
     <ImageStyle src={photo} alt="zoomed-pet" />
+    <ZoomedNextStyle />
+    <ZoomedCloseStyle />
   </PortalStyle>
 );
 
