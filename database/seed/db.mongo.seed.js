2cc7e2d1e611f204c646333b160e2b883746a873
Peter Vu <pvu@stripe.com>
2020-10-16 23:48:14 -0700

    Updated mongo seeding file to enable batching of 100K record

--- database/seed/db.mongo.seed.js
+++ database/seed/db.mongo.seed.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const path = require('path');
+const JSONStream = require('JSONStream');
+const eventStream = require('event-stream');
 
 const fsPromise = fs.promises;
 const {
@@ -9,29 +12,40 @@ const {
   emptyCollection,
 } = require('../dbConnection.js');
 
-
 // build array of data to insert into db
-const pictureData = [];
-let numOfImages = 0;
-let randomImage = 0;
-let imgUrl = '';
+// const pictureData = [];
+// let numOfImages = 0;
+// let randomImage = 0;
+// let imgUrl = '';
 
 // delete all records from database if exists
 // eslint-disable-next-line no-console
-emptyCollection((err) => console.log('Error Deleting Collection', err));
 
-const seedImages = async function seedImagesIntoMongo(modelName, file) {
-  
+const seedImages = function seedImagesIntoMongo(file, modelName) {
+  emptyCollection(modelName, (err) => console.log('Error Deleting Collection', err));
+
+  const readStream = fs.createReadStream(file, {
+    encoding: 'utf-8',
+  });
+  const json = readStream.on('data', () => {}).pipe(JSONStream.parse('*'));
+
+  let documentUploadCount = 0;
+  let documentBatchArray = [];
+  json.pipe(eventStream.map((document) => {
+    documentBatchArray.push(document);
+
+    if (documentBatchArray.length % 100000 === 0) {
+      modelName.insertMany(documentBatchArray, (error, docs) => {
+        if (error) {
+          return error;
+        }
+        documentBatchArray = [];
+        documentUploadCount += 100000;
+        console.log(docs);
+        console.log(`Inserted ${documentUploadCount} documents into database`);
+      });
+    }
+  }));
 };
 
-// after 100 database records have been built, add to database
-Picture.insertMany(pictureData, (error) => {
-  if (error) {
-    // eslint-disable-next-line no-console
-    console.log('error inserting into db:', error);
-  } else {
-    // eslint-disable-next-line no-console
-    console.log('documents inserted');
-    db.close();
-  }
-});
+seedImages(path.join(__dirname, 'reviewPictures.json'), ReviewImage);
