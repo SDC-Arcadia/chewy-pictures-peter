991fba4ba59d7ef8d95c95d3a1dab8a3560d81c7
Peter Vu <pvu@stripe.com>
2020-11-06 23:40:50 -0800

    Set up initial K6 with new relic, and first stress test file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// const newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -26,30 +27,13 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded());
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-// Need to refactor database completely to reduce database processes
+// product image crud endpoints
 app.get('/photos/:productId', getProductPictures);
 app.post('/photos/:productId', createProductPictures);
 app.delete('/photos/:productId/:pictureId', deleteProductPicture);
 app.patch('/photos/:productId/:pictureId', updateProductPicture);
 
-// review photos crud endpoints
-
-// app.get('/review-photos/:productId', (req, res) => {
-//   const product = req.params.productId;
-//   // query db for product
-//   queryProduct(product, (err, result) => {
-//     if (err) {
-//       res.statusCode = 400;
-//       res.send({ error: err });
-//     } else if (!result) {
-//       res.statusCode = 404;
-//       res.send({ error: 'record not found' });
-//     } else {
-//       res.send(buildApiResponse(result, 'reviews'));
-//     }
-//   });
-// });
-
+// review image crud endpoints
 app.get('/review-photos/:productId', getReviewPictures);
 app.post('/review-photos/:productId', createReviewPictures);
 app.delete('/review-photos/:productId/:pictureId', deleteReviewPicture);
